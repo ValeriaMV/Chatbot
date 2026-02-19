a85202b4a81c19b16aab07b6be443c69c07899a6
ValeriaMV <142053905+ValeriaMV@users.noreply.github.com>
2026-02-19 13:29:47 -0600

    Webhook

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 import json
@@ -20,13 +20,6 @@ class Log(db.Model):
 with app.app_context():
     db.create_all()
 
-    prueba1 = Log(texto="Mensaje de Prueba 1")
-    prueba2 = Log(texto="Mensaje de Prueba 2")
-
-    db.session.add(prueba1)
-    db.session.add(prueba2)
-    db.session.commit()
-
 #Función para ordenar los registros por fecha y hora
 def ordenar_por_fecha_y_hora(registros):
     return sorted(registros, key=lambda x: x.fecha_y_hora, reverse=True)
@@ -42,12 +35,40 @@ mensajes_log = []
 
 #Función para agregar mensajes y guardar en la base de datos
 def agregar_mensajes_log(texto):
+    
     mensajes_log.append(texto)
 
     #Guardar el mensaje en la bse de datos
     nuevo_registro = Log(texto=texto)
     db.session.add(nuevo_registro)
     db.session.comit()
+
+#Token de verificación para la configuración
+TOKEN_VALCODE = "VALCODE"
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        challenge = verificar_token(request)
+        return challenge
+    elif request.method == 'POST':
+        reponse = recibir_mensajes(request)
+        return reponse
+
+def verificar_token(req):
+    token = req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if challenge and token == TOKEN_VALCODE:
+        return challenge
+    else:
+        return jsonify({'error': 'Token Inválido'}), 401
+
+def recibir_mensajes(req):
+    req = request.get_json()
+    agregar_mensajes_log(req)
+    
+    return jsonify({'message': 'EVENT_RECEIVED'})
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
