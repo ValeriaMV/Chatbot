d4f1f6a273c7127ce7e5d554ab2cb9076bbe8d6d
ValeriaMV <142053905+ValeriaMV@users.noreply.github.com>
2026-03-05 13:13:16 -0600

    Webhook

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ def recibir_mensajes(req):
     return jsonify({'message': 'EVENT_RECEIVED'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
